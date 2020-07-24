cd1da140d8837ebeac17ec03d2fc3a13dc67c3de
Lewis Wear <lewiswear@Lewiss-MBP.broadband>
2020-07-24 22:16:40 +0100

    Updates

---
+++ precache-manifest.de050a9a5709db86d8a6f288a99c255e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d5941604ccc6588c22441dfda0b7f8",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "3f7043c99b10e7a4bd0d",
+    "url": "/movie-db/static/js/2.0f8293ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-db/static/js/2.0f8293ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4871b8e0d4649fd722be",
+    "url": "/movie-db/static/js/main.1a61b49d.chunk.js"
+  },
+  {
+    "revision": "1e35b73533c8089232bd",
+    "url": "/movie-db/static/js/runtime-main.58960b7a.js"
+  }
+]);
